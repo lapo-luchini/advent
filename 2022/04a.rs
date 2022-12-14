7dfa6c54c828617e15c1d4d2d1b337331f76f4b4
Lapo Luchini <lapo@lapo.it>
2022-12-14 00:12:53 +0000

    Solved 2022/4a.

---
+++ 2022/04a.rs
@@ -0,0 +1,14 @@
+use std::{io, io::prelude::*};
+fn main() {
+    let mut num: i32 = 0;
+    for line in io::stdin().lock().lines() {
+        let a: Vec<i32> = line.unwrap()
+            .split(|c| c == '-' || c == ',')
+            .flat_map(|x| x.parse()).collect();
+        if (a[0] <= a[2] && a[1] >= a[3]) ||
+           (a[0] >= a[2] && a[1] <= a[3]) {
+            num = num + 1;
+        }
+        println!("{}-{},{}-{}={}", a[0], a[1], a[2], a[3], num);
+    }
+}
